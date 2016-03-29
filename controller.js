7ea0e14c8560b94ce75abd55443e78c33d97e2f1
curtjenk <curtjenk@comcast.net>
2016-03-29 10:48:23 -0400

    Added country flag images and dropdown for countries

--- controller.js
+++ controller.js
@@ -1,31 +1,34 @@
-
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myController', function($scope) {
 
+//these are in the country-code-lookup.js
+    $scope.isoCountries = isoCountries;
+    $scope.convertCountryCode = convertCountryCode;
+//-------------------------
+
     $scope.countries = [];
 
-    function Country(name, pop, leader, flagColors) {
+    function Country(name, pop, leader, flagColors, flagImgSrc) {
         this.name = name;
         this.population = pop;
         this.leader = leader;
         this.flagColors = flagColors;
+        this.flagImgSrc = flagImgSrc;
     }
 
     Country.prototype.toString = function() {
         return this.name + 'has ' + this.population + ' people and is lead by ' + this.leader;
     };
 
-//ng-blur
+    //ng-blur
     $scope.dataChange = function() {
-        console.log($scope.name.length);
-        console.log($scope.population.length);
-        console.log($scope.leader.length);
-        console.log($scope.flagColors.length);
+        // console.log($scope.name.length);
+        // console.log($scope.population.length);
+        // console.log($scope.leader.length);
+        // console.log($scope.flagColors.length);
         if ($scope.name.length > 0 && $scope.population.length > 0 && $scope.leader.length > 0 && $scope.flagColors.length > 0) {
-            // && $scope.flagColors > 0) {
-            // alert('here');
-            var newCountry = new Country($scope.name, $scope.population, $scope.leader, $scope.flagColors);
+            var newCountry = new Country($scope.convertCountryCode($scope.name), $scope.population, $scope.leader, $scope.flagColors);
             $scope.countries.push(newCountry);
             $scope.name = '';
             $scope.population = '';
@@ -35,8 +38,16 @@ myApp.controller('myController', function($scope) {
     };
 
     $scope.addCountry = function() {
-        // alert('here');
-        var newCountry = new Country($scope.name, $scope.population, $scope.leader, $scope.flagColors);
-        $scope.countries.push(newCountry);
+        $scope.dataChange();
+    };
+
+    //passing $index
+    $scope.removeCountry = function(n) {
+        // console.log("remove country");
+        $scope.countries.splice(n, 1);
+    };
+
+    $scope.getFlag = function(code) {
+        $scope.flag="flags/1x1/" + code + ".svg";
     };
 });
